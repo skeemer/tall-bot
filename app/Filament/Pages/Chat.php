664bdf445fadbe850d6dc8a1a20afd87b94fad45
Leo Lutz <leo@leolutz.com>
2026-02-19 22:43:53 -0700

    Add navigation icon to Chat page

--- app/Filament/Pages/Chat.php
+++ app/Filament/Pages/Chat.php
@@ -2,12 +2,16 @@
 
 namespace App\Filament\Pages;
 
+use BackedEnum;
 use Filament\Pages\Page;
+use Filament\Support\Icons\Heroicon;
 use Illuminate\Contracts\View\View;
 
 class Chat extends Page
 {
     protected string $view = 'filament.pages.chat';
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedChatBubbleLeftRight;
 
     public function getHeader(): ?View
     {
